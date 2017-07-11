f3568061db5645ac873d1326520564c65cd6bdd5
Amy <purumskyamy@gamil.com>
2017-07-11 21:27:00 +0900

    초기 버전

---
+++ src/edu/FirstServlet.java
@@ -0,0 +1,15 @@
+package edu;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+@Override
+protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+	System.out.println("service() »£√‚");
+}
+}
